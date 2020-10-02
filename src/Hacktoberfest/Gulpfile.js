e954278247cf74755ebffece9399513b41c42191
Willem Meints <1550763+wmeints@users.noreply.github.com>
2020-10-02 11:42:09 +0200

    Add content for the website

--- src/Hacktoberfest/Gulpfile.js
+++ src/Hacktoberfest/Gulpfile.js
@@ -5,4 +5,8 @@ exports.compileStylesheets = function compileStylesheets() {
     return gulp.src('Stylesheets/site.scss').pipe(sass({ outputStyle: 'compressed' })).pipe(gulp.dest('wwwroot/css'));
 };
 
+exports.watch = function watch() {
+    gulp.watch(['Stylesheets/**/*'], exports.compileStylesheets);
+};
+
 exports.default = gulp.series([exports.compileStylesheets]);
